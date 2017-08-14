791580437f4fb8c244f5f75f4a4e5ed20b61a9af
Rachael Boddy <bodd113@hotmail.co.uk>
2017-08-14 12:21:53 +0100

    done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,9 @@ class QuestionsController < ApplicationController
   def answer
     if params[:query]
         @your_message = params[:query]
-      if @your_message[-1] == "?"
+      if @your_message= "I am going to work right now!"
+        @answer = ""
+      elsif @your_message[-1] == "?"
         @answer = "Silly question, get dressed and go to work!"
       else
         @answer = "I don't care, get dressed and go to work!"
